320a82f13f3a780a65c1e8fe6ed246bb73e856c1
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-03-19 22:48:15 +0600

    Add clearScreen function and improve user interface

--- main.cpp
+++ main.cpp
@@ -26,10 +26,26 @@ void findPerfectNumbers(int limit) {
     }
 }
 
+void clearScreen() {
+    cout << "\033[2J\033[1;1H";
+}
+
 int main() {
+    cout << "A perfect number is a positive integer that is equal to the sum of its proper divisors." << endl;
+
     int limit;
-    cout << "Enter the limit: ";
+    cout << "Enter the limit: (Enter -1 to run forever)" << endl;
     cin >> limit;
+    clearScreen();
+
+    cout << "Looking for perfect numbers up to ";
+    if (limit == -1) {
+        cout << "infinity" << endl;
+    } else {
+        cout << limit << endl;
+    }
+    cout << "Press Ctrl + C to stop" << endl << endl;
+
     findPerfectNumbers(limit);
     return 0;
 }
